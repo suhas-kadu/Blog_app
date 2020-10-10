8640694465ff6bf62c8172978d5782b2853f24be
Suhas Kadu <kadusuhas72@gmail.com>
2020-10-10 15:50:17 +0530

    second commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,19 +1,5 @@
 import 'package:blog_app/views/blog_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
-
-const String _content = """
-# blog_app
-A new Flutter project.
-## Getting Started
-This project is a starting point for a Flutter application.
-A few resources to get you started if this is your first Flutter project:
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
-""";
 
 class HomePage extends StatefulWidget {
   @override
@@ -24,7 +10,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Blogs")),
+      appBar: AppBar(title: Text("Blog")),
       body: Container(
           child: BlogScreen(),
         ),
--- lib/views/blog_screen.dart
+++ lib/views/blog_screen.dart
@@ -1,11 +1,38 @@
-import 'dart:io';
+//import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'user_profile.dart';
 
 const String _contents = """
+# A new Flutter project.
+## Getting Started
+This project is a starting point for a Flutter application.
+A few resources to get you started if this is your first Flutter project:
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
 # blog_app
+
 A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+![image](https://miro.medium.com/max/700/1*TkNd1PwwwdBi9Z3kdG5Hng.png)
+![image]()
 ## Getting Started
 This project is a starting point for a Flutter application.
 A few resources to get you started if this is your first Flutter project:
@@ -34,42 +61,166 @@ samples, guidance on mobile development, and a full API reference.
 ![image](https://miro.medium.com/max/700/1*TkNd1PwwwdBi9Z3kdG5Hng.png)
 ![image]()
 
+## Getting Started
+This project is a starting point for a Flutter application.
+A few resources to get you started if this is your first Flutter project:
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+# blog_app
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+![image](https://miro.medium.com/max/700/1*TkNd1PwwwdBi9Z3kdG5Hng.png)
+![image]()
+
+# test_app
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
 
 """;
 
-class BlogScreen extends StatelessWidget {
+class BlogScreen extends StatefulWidget {
+  @override
+  _BlogScreenState createState() => _BlogScreenState();
+}
+
+class _BlogScreenState extends State<BlogScreen> {
+  int _counter = 0;
+  //void _incrementCounter() {
+    //setState(() {
+      //_counter += 1;
+    //});
+  //}
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setState(() {
+      //_incrementCounter();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white70,
       body: Container(
         child: FutureBuilder(
-            future: Future.delayed(Duration(seconds: 1), () => rootBundle.loadString("assets/file.md")),
+            future: Future.delayed(Duration(seconds: 1),
+                () => rootBundle.loadString("assets/file.md")),
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return Container(
-                  child: Column(
-                    children: <Widget>[
-                         Container(
-                           child: Center(
-                            child: Text("My First Flutter Blog",
-                            style: TextStyle(fontSize: 30),
+                return SingleChildScrollView(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.all(8),
+                          child: Center(
+                            child: Text(
+                              "My First Flutter Blog",
+                              style: TextStyle(fontSize: 30),
                             ),
+                          ),
                         ),
-                         ),
-
-                      Container(child: Center(child: Text("Suhas Kadu", style: TextStyle(color: Colors.blue),))),
-                      Expanded(
-                        child: Markdown(
-                          data: _contents,
-                          imageDirectory: Directory("assets/images/logo.png", ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => UserProfile()));
+                          },
+                          child: Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin: const EdgeInsets.all(8),
+                              child: Center(
+                                  child: Text(
+                                "Suhas Kadu",
+                                style: TextStyle(color: Colors.blue),
+                              ))),
                         ),
-                      ),
-
-                    ],
+                        Container(
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 8, vertical: 8),
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          child: MarkdownBody(
+                            data: _contents,
+                            //imageDirectory: Directory("assets/images/logo.png",),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => UserProfile()));
+                          },
+                          child: Container(
+                            //margin: const EdgeInsets.symmetric(horizontal: 8),
+                            padding: const EdgeInsets.symmetric(horizontal: 8),
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                  backgroundImage:
+                                      AssetImage("assets/images/logo.png")),
+                              title: Text("Suhas Kadu"),
+                              subtitle: Text("TRF"),
+                            ),
+                          ),
+                        ),
+                        /*
+                        Container(
+                            child: Row(
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.favorite_border),
+                              onPressed: () {
+                                _incrementCounter();
+                              },
+                              tooltip: 'Increment',
+                            ),
+                            Text("$_counter"),
+                          ],
+                        ),
+                        ),
+                        */
+                      ],
+                    ),
                   ),
                 );
               }
-
               return Center(
                 child: CircularProgressIndicator(),
               );
